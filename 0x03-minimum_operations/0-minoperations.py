75bc332460e165d49e06410824d062d94a42837e
Axel <axel.valentinbvs@yahoo.com>
2023-01-04 12:40:32 +0000

    pep8

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -3,18 +3,18 @@
 
 
 def minOperations(n):
-	"""
-		calc min operations to reach n  
-	"""
-	if n <= 1:
-		return 0
+    """
+            calc min operations to reach n
+    """
+    if n <= 1:
+        return 0
 
-	index = 2
-	count = 0
-	while index <= n:
-		if n % index == 0:
-			count += index
-			n /= index
-		else:
-			index += 1
-	return count
+    index = 2
+    count = 0
+    while index <= n:
+        if n % index == 0:
+            count += index
+            n /= index
+        else:
+            index += 1
+    return count
