c376ae129c82c811b3c099af1e78276cbcae0029
Axel <axel.valentinbvs@yahoo.com>
2023-01-04 12:20:37 +0000

    use tab

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-
 """0x03. Minimum Operations"""
 
 
 def minOperations(n):
-    """
-        calc min operations to reach n  
-    """
-    if n <= 1:
-        return 0
+	"""
+		calc min operations to reach n  
+	"""
+	if n <= 1:
+		return 0
 
-    index = 2
-    count = 0
-    while index <= n:
-        if n % index == 0:
-            count += index
-            n /= index
-        else:
-            index += 1
-    return count
+	index = 2
+	count = 0
+	while index <= n:
+		if n % index == 0:
+			count += index
+			n /= index
+		else:
+			index += 1
+		return count
