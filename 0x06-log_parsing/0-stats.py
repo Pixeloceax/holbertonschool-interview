7550f142ff882ca22a0bf103286012f35f843540
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-01-26 16:09:16 +0000

    add task

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -5,25 +5,30 @@ from sys import stdin
 total_size = 0
 status_codes = {code: 0 for code in [
     "200", "301", "400", "401", "403", "404", "405", "500"]}
-count = 0
 
-for line in stdin:
-    try:
-        args = line.strip().split(" ")
-        if len(args) == 9:
-            if args[-2] in status_codes:
-                status_codes[args[-2]] += 1
-            total_size += int(args[-1])
-            count += 1
-            if count % 10 == 0:
-                print("File size: {}".format(total_size))
-                for status, count in sorted(status_codes.items()):
-                    if count != 0:
-                        print("{}: {}".format(status, count))
-    except ValueError:
-        pass
 
-print("File size: {}".format(total_size))
-for status, count in sorted(status_codes.items()):
-    if count != 0:
-        print("{}: {}".format(status, count))
+def print_():
+    """Prints the metrics"""
+    print("File size: {}".format(total_size))
+    for status, count in sorted(status_codes.items()):
+        if count != 0:
+            print("{}: {}".format(status, count))
+
+
+try:
+    for i, line in enumerate(stdin, start=1):
+        matches = line.rstrip().split()
+        try:
+            status_code = matches[-2]
+            file_size = matches[-1]
+            if status_code in status_codes.keys():
+                status_codes[status_code] += 1
+            total_size.append(int(file_size))
+        except Exception:
+            pass
+        if (i % 10 == 0):
+            print_()
+    print_()
+except KeyboardInterrupt:
+    print_()
+    raise
