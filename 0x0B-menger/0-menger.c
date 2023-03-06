56d4903a7e4d72dd7fe12ffb794c7f3a1c0e486f
Axel Valentin <axel.valentinbvs@yahoo.com>
2023-03-06 12:38:16 +0000

    Add files for Menger sponge project

---
+++ 0x0B-menger/0-menger.c
@@ -0,0 +1,40 @@
+#include "menger.h"
+
+/**
+ *
+ *
+ * @param level
+ */
+
+void menger(int level)
+{
+	int total_size = pow(3, level);
+	int i, j, x, y, is_empty;
+
+	for (i = 0; i < total_size; i++)
+	{
+		for (j = 0; j < total_size; j++)
+		{
+			is_empty = 0;
+			x = i;
+			y = j;
+
+			while (x > 0 || y > 0)
+			{
+				if (x % 3 == 1 && y % 3 == 1)
+				{
+					is_empty = 1;
+					break;
+				}
+				x /= 3;
+				y /= 3;
+			}
+
+			if (is_empty == 1)
+				printf(" ");
+			else
+				printf("#");
+		}
+		printf("\n");
+	}
+}
---
+++ 0x0B-menger/menger.h
@@ -0,0 +1,10 @@
+#ifndef MENGER_H
+#define MENGER_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+void menger(int level);
+
+#endif /* MENGER_H_ */
